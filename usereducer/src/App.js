0f96e972bbdebd2e7966bb36e6217d3fcf9a49e4
webdeveloper1213 <sameera_sulaiman@yahoo.com>
2023-01-04 19:39:05 -0800

    commented unnecessary code

--- usereducer/src/App.js
+++ usereducer/src/App.js
@@ -4,6 +4,8 @@ import useFetch from "./useFetch";
 
 function App() {
   const BASE_URL = "https://inshortsapi.vercel.app/news?category=all";
+  // const BASE_URL =  "https://jsonplaceholder.typicode.com/users";
+
   const { isLoading, error, data:users} = useFetch(BASE_URL);
    console.log(users,"USERS")
   return (
@@ -11,14 +13,14 @@ function App() {
       <h2>useReducer() Example Fetching data using custom useFetch hook</h2>
       {isLoading && <p style={{display:"flex",justifyContent:"center",alignItems:"center"}}>Loading...</p>}
       {error && <p style={{display:"flex",justifyContent:"center",alignItems:"center"}}>Error</p>}
-      {/* {data ? data.map((user) => {
-        return (
-          <div key={user.id}>
-           <p>{user.name}</p>
-          </div>
-        )
-      }) : "NOT FOUND"} */}
-       <div>
+ {/* <ul>
+ {users.map((user) => {
+    return (
+      <li key={user.id}>{user.name}</li>
+    )
+  })}
+ </ul> */}
+      <div> 
         <pre>{JSON.stringify(users, undefined, 2)}</pre> 
         {/* syntax JSON.stringify(data , function ,spaces); */}
       </div>
